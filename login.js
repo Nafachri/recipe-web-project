f23436b1f1626538d63d24c1825b8430415ddda3
Oktado Putra <oktado.putra@gmail.com>
2020-11-11 10:46:51 +0700

    menambah fitur invalid username

--- login.js
+++ login.js
@@ -1,6 +1,5 @@
 import { storeUserCredential } from "./services/local-storage-services.js";
 
-let errorMessage = "Invalid username or password.";
 const button = document.getElementById("button-sign-in");
 button.addEventListener("click", validate);
 
@@ -9,21 +8,25 @@ async function validate() {
     const username = document.getElementById("username");
     const password = document.getElementById("password");
 
+    let errorMessage = "Invalid username or password.";
+
     const response = await fetch(
       "https://5fa790be9def160016adaf1c.mockapi.io/api/v1/users"
     );
     const data = await response.json();
-    console.log(data);
     for (let i = 0; i < data.length; i++) {
       if (
         username.value === data[i].username &&
         password.value === data[i].password
       ) {
         let isStored = storeUserCredential(data[i]);
-        console.log(isStored);
+        console.log("login success");
         if (isStored) {
           window.location.href = "./findrecipepage.html";
         }
+      } else {
+        document.getElementById("error").innerHTML = errorMessage;
+        console.log("error");
       }
     }
   } catch (error) {
